165a6b668e0b514a5b729645d767cddc49984012
RadicalEddward <RadicalEddward@protonmail.com>
2025-07-05 10:17:34 +0200

    finish basic APIs to DWDS and display results

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
@@ -18,6 +19,45 @@ app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 })
 
+// DWDS API
+app.get("/dwds", async (req, res) => {
+  const userInput = req.query.word.split(" ");
+  const word = userInput.join("|").trim();
+  console.log(`Searching for: ${word}`);
+
+  // DWDS API configuration (usually just word as a query parameter)
+  const confiDWDS = {
+    params: {
+      q: word
+    }
+  }
+  // DWDS API endpoints
+  let endpoints = [
+    "https://www.dwds.de/api/wb/snippet/",
+    "https://www.dwds.de/api/frequency/",
+  ]
+
+  // get the results from the DWDS API
+  try {
+    const results = await Promise.all(endpoints.map((endpoint) => {
+      return axios.get(endpoint, confiDWDS);
+    }))
+
+    res.render("index.ejs", {
+      dwdsData: results[0].data,
+      dwdsFrequency: results[1].data,
+    });
+  } catch (error) {
+    console.error("Error fetching data from DWDS API:", error);
+    res.render("index.ejs", {
+      dwdsData: null,
+      dwdsFrequency: null,
+      error: "Error fetching data from DWDS API"
+    });
+  }
+
+
+})
 
 
 
